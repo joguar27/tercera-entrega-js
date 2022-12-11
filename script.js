c5d8d6e26e022c4450c88e9ca8f5096975243c6d
jorge <jorgegusarruda@gmail.com>
2022-12-11 02:10:40 -0300

    Agregué jsonPlaceholder y fetch

--- script.js
+++ script.js
@@ -159,6 +159,49 @@ Toastify({
 }).showToast();
 
 
+//JsonPlaceholder
+console.log(fetch('https://jsonplaceholder.typicode.com/posts')) //promesa
+
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+.then((resp) => console.log(resp)) //promesa resuelta
+
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+.then((response) => response.json ()) //transforma la info
+.then((json) => console.log(json)); //respuesta
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+.then((response) => response.json ())
+.then((data) => {
+  console.log(data[0].title) //me muestra contenido del titulo
+  console.log(data[0].body) //me muestra contenido del cuerpo
+});
+
+
+const lista = document.querySelector('#listado')
+fetch('https://jsonplaceholder.typicode.com/posts')
+.then((resp)=>resp.json())
+.then((data)=>{
+  data.forEach((post=>{
+    const li=document.createElement('li')
+    li.innerHTML=`
+    <h4>${post.title}</h4>
+    <p>${post.body}</p>`
+    lista.append(li)
+  }))
+})
+
+
+
+
+
+
+
+
+
+
+
 
 
 
